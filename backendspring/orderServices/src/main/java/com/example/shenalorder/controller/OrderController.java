e86668171dc0536e2d94a2e646e0c45ba2f43dbf
Dinuth Rashmika <dinuthwelandagoda@gmail.com>
2025-04-29 23:22:32 +0530

    fixed error

--- backendspring/orderServices/src/main/java/com/example/shenalorder/controller/OrderController.java
+++ backendspring/orderServices/src/main/java/com/example/shenalorder/controller/OrderController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/orders")
 public class OrderController {
 
